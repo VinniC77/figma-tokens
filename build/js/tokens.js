8b1dd396e2e720fb29def54c3427115bd38ddf75
viniciusClunc <vinicius@valorizza.com.br>
2022-12-08 13:30:58 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 08 Dec 2022 16:27:47 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
